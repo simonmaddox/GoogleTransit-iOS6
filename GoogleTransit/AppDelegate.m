2504a7d4e5442fc21154139fd59bfa6ba6f52b01
Simon Maddox <simon@simonmaddox.com>
2012-09-20 13:34:01 +0100

    First commit

--- GoogleTransit/AppDelegate.m
+++ GoogleTransit/AppDelegate.m
@@ -7,43 +7,39 @@
 //
 
 #import "AppDelegate.h"
+#import <MapKit/MapKit.h>
+
+// http://maps.google.com/maps?f=d&source=s_d&saddr=euston+station&daddr=air+street&hl=en&geocode=FchBEgMd9vT9_ykhn8M0JBt2SDH3lGPQqsbmFg%3BFVT6EQMd1Ov9_ymjHB0Z1AR2SDFjlguRha5tmA&vps=3&jsv=432b&sll=51.518765,-0.139845&sspn=0.019601,0.093813&vpsrc=0&gl=us&dirflg=r&ttype=now&noexp=0&noal=0&sort=def&mra=atm&ie=UTF8&ui=maps_mini&cad=tm:d
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+	if ([MKDirectionsRequest isDirectionsRequestURL:url]){
+		
+		MKDirectionsRequest *directionsRequest = [[MKDirectionsRequest alloc] initWithContentsOfURL:url];
+		
+		NSString *directionsURLString = [NSString stringWithFormat:@"https://maps.google.com/maps?f=d&source=s_d&saddr=%f,%f&daddr=%f,%f&hl=en&vps=3&jsv=432b&vpsrc=0&gl=us&dirflg=r&ttype=now&noexp=0&noal=0&sort=def&mra=atm&ie=UTF8&ui=maps_mini",
+										 directionsRequest.source.placemark.location.coordinate.latitude,
+										 directionsRequest.source.placemark.location.coordinate.longitude,
+										 directionsRequest.destination.placemark.location.coordinate.latitude,
+										 directionsRequest.destination.placemark.location.coordinate.longitude];
+		
+		NSURL *directionsURL = [NSURL URLWithString:directionsURLString];
+		
+		[[UIApplication sharedApplication] openURL:directionsURL];
+		
+		return YES;
+	}
+	return NO;
 }
 
 @end
