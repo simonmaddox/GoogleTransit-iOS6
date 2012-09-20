32ddedaed711bd29581756574a94ae7ae2cb672a
Simon Maddox <simon@simonmaddox.com>
2012-09-20 12:45:52 +0100

    Initial Commit

---
+++ GoogleTransit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleTransit
+//
+//  Created by Simon Maddox on 20/09/2012.
+//  Copyright (c) 2012 Simon Maddox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoogleTransit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoogleTransit
+//
+//  Created by Simon Maddox on 20/09/2012.
+//  Copyright (c) 2012 Simon Maddox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
